d7a8e36f7eeab19e1455ed724cb278ad1b8f0428
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2021-03-24 01:57:30 +0900

    compile ok

--- ch2/umat/main.cpp
+++ ch2/umat/main.cpp
@@ -1,21 +1,21 @@
-#include <string>
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#pragma(lib, "opencv_world400.lib");
+#include <string>
 
 using namespace cv;
 using namespace std;
 
 int main(int argc, char *argv[]) {
 
-    string path = "../../image/sample.jpg";
-  {
-    Mat mat imread(path);
-    UMat umat;
-    mat.coppyTo(umat);
-  }
-  {
-    UMat umat;
-    imread(path).coppyTo(umat);
-  }
+  string path = "../../images/sample.jpg";
+  Mat src, dst;
+  src = imread(path);
+  cvtColor(src, dst, COLOR_RGB2GRAY);
+  imshow("src", src);
+  imshow("dst", dst);
 
+  waitKey(0);
 }
